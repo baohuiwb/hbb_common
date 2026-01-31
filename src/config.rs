312db91481298ffc50864ff323f3cd9b98d034db
baohuiwb <baohuiwb@users.noreply.github.com>
2026-01-31 22:11:38 +0800

    开启 incoming-only 模式

--- src/config.rs
+++ src/config.rs
@@ -2495,11 +2495,8 @@ fn is_option_can_save(
 
 #[inline]
 pub fn is_incoming_only() -> bool {
-    HARD_SETTINGS
-        .read()
-        .unwrap()
-        .get("conn-type")
-        .map_or(false, |x| x == ("incoming"))
+    // 强制启用 incoming-only 模式，只显示 ID
+    true
 }
 
 #[inline]
